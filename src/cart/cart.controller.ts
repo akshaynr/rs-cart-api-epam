3215292dd202beb866fcadce5d1df54b7a4e24a9
Akshay Raskar <anr241@gmail.com>
2023-05-03 16:43:54 +0530

    removed logic for deleting record from cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -81,7 +81,7 @@ export class CartController {
       items,
       total,
     });
-    await this.cartService.removeByUserId(userId);
+    // await this.cartService.removeByUserId(userId);
 
     return {
       statusCode: HttpStatus.OK,
